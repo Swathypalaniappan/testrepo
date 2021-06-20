3b17491a3e0964d2475750dbf01a2d13d9ded147
Swathypalaniappan <swathypalani2000@gmail.com>
2021-06-20 06:27:08 +0000

    changes mad from theia lab environment

--- testchild.py
+++ testchild.py
@@ -1,2 +1,4 @@
 ##Adding new file in child branch
 print("Inside child branch")
+
+Print("Updated in the theia environment")
---
+++ theiaFile.py
@@ -0,0 +1,2 @@
+# this file is added in the theia environment
+print("This is new file which was added in the theia environment")
